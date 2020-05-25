a14cb30f3a76161aca825efc8edcb259949e7e7a
Ryan S. DeFever <r.s.defever@gmail.com>
2020-05-25 13:29:15 -0400

    Update plot font sizes

--- simple-examples/npt_methane/ana.py
+++ simple-examples/npt_methane/ana.py
@@ -1,4 +1,5 @@
 import unyt as u
+import numpy as np
 import matplotlib.pyplot as plt
 
 from mosdef_cassandra.analysis import ThermoProps
@@ -9,10 +10,10 @@ plt.plot(
     thermo.prop("MC_STEP").value,
     thermo.prop("Pressure").to_value(u.MPa),
 )
-plt.xlabel("MC Step", fontsize=16, labelpad=10)
-plt.ylabel("Pressure (MPa)", fontsize=16, labelpad=10)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
+plt.xlabel("MC Step", fontsize=24, labelpad=10)
+plt.ylabel("Pressure (MPa)", fontsize=24, labelpad=10)
+plt.xticks(np.arange(0,250001,75000), fontsize=16)
+plt.yticks(fontsize=16)
 plt.tight_layout()
 
 plt.savefig("pressure.pdf")
