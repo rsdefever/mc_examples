44083c249fb6fa893cd4c1ac090fcaa633a34781
Ryan S. DeFever <r.s.defever@gmail.com>
2020-05-25 13:21:02 -0400

    Add GEMC files

---
+++ simple-examples/gemc_methane/ana.py
@@ -0,0 +1,42 @@
+import unyt as u
+import matplotlib.pyplot as plt
+import numpy as np
+
+from mosdef_cassandra.analysis import ThermoProps
+
+thermo_liq = ThermoProps("gemc.out.box1.prp")
+thermo_vap = ThermoProps("gemc.out.box2.prp")
+
+plt.plot(
+    thermo_vap.prop("MC_STEP").value,
+    thermo_vap.prop("Pressure").to_value(u.kPa),
+)
+
+plt.xlabel("MC Step", fontsize=24, labelpad=20)
+plt.ylabel("Vapor pressure (kPa)", fontsize=24, labelpad=20)
+plt.xticks(np.arange(0, 500001, 125000), fontsize=16)
+plt.yticks(fontsize=16)
+plt.tight_layout()
+
+plt.savefig("pressure.pdf")
+plt.close()
+
+plt.plot(
+    thermo_vap.prop("MC_STEP").value,
+    thermo_vap.prop("Mass_Density").to_value('kg/m**3'),
+    label="Vapor",
+)
+plt.plot(
+    thermo_liq.prop("MC_STEP").value,
+    thermo_liq.prop("Mass_Density").to_value('kg/m**3'),
+    label="Liquid",
+)
+
+plt.xlabel("MC Step", fontsize=24, labelpad=20)
+plt.ylabel("Density (kg/m$\mathregular{^3}$)", fontsize=24, labelpad=20)
+plt.xticks(np.arange(0, 500001, 125000), fontsize=16)
+plt.yticks(fontsize=16)
+plt.legend(fontsize=24)
+plt.tight_layout()
+
+plt.savefig("density.pdf")
---
+++ simple-examples/gemc_methane/gemc.py
@@ -0,0 +1,51 @@
+import mbuild
+import foyer
+import unyt as u
+import mosdef_cassandra as mc
+
+# Load the force field from foyer
+ff = foyer.forcefields.load_TRAPPE_UA()
+
+# Create a single site methane
+methane = mbuild.Compound(name="_CH4")
+
+# Apply the force field
+methane_ff = ff.apply(methane)
+
+# Initial vapor box based IG law @ 140 K, 1 bar
+N_vapor = 200
+pressure = 1.0 * u.bar
+temperature = 110 * u.K
+vapor_volume = N_vapor * u.kb * temperature / pressure
+box_length = (vapor_volume ** (1./3.)).to_value(u.nm)
+vapor_box = mbuild.Box([box_length, box_length, box_length])
+
+# Initial liquid box from existing configuration
+liquid_box = mbuild.load("liqbox_equil.gro")
+N_liquid = liquid_box.n_particles
+
+# Create the System and MoveSet
+box_list = [liquid_box, vapor_box]
+species_list = [methane_ff]
+mols_in_boxes = [[N_liquid], [0]]
+mols_to_add = [[0], [N_vapor]]
+
+system = mc.System(
+    box_list,
+    species_list,
+    mols_in_boxes=mols_in_boxes,
+    mols_to_add=mols_to_add
+)
+moveset = mc.MoveSet("gemc", species_list)
+
+# Run the Monte Carlo simulation
+mc.run(
+    system=system,
+    moveset=moveset,
+    run_type="equilibration",
+    run_length=500000,
+    temperature=temperature,
+    charge_style="none",
+    vdw_cutoff=14.0 * u.angstrom,
+)
+
