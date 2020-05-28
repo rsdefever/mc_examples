c5c603705fee7bf1f8bc56168c283b5977a04fee
Ryan S. DeFever <r.s.defever@gmail.com>
2020-05-28 15:35:04 -0400

    Add complex examples

--- simple-examples/gemc_methane/ana.py
+++ simple-examples/gemc_methane/ana.py
@@ -10,6 +10,7 @@ thermo_vap = ThermoProps("gemc.out.box2.prp")
 plt.plot(
     thermo_vap.prop("MC_STEP").value,
     thermo_vap.prop("Pressure").to_value(u.kPa),
+    color="#2aa134",
 )
 
 plt.xlabel("MC Step", fontsize=24, labelpad=20)
@@ -25,18 +26,20 @@ plt.plot(
     thermo_liq.prop("MC_STEP").value,
     thermo_liq.prop("Mass_Density").to_value('kg/m**3'),
     label="Liquid",
+    color="#2aa134",
 )
 plt.plot(
     thermo_vap.prop("MC_STEP").value,
     thermo_vap.prop("Mass_Density").to_value('kg/m**3'),
     label="Vapor",
+    color="#4b85b3"
 )
 
 plt.xlabel("MC Step", fontsize=24, labelpad=20)
 plt.ylabel("Density (kg/m$\mathregular{^3}$)", fontsize=24, labelpad=20)
 plt.xticks(np.arange(0, 500001, 125000), fontsize=16)
 plt.yticks(fontsize=16)
-plt.legend(fontsize=24)
+plt.legend(loc="center right", fontsize=24)
 plt.tight_layout()
 
 plt.savefig("density.pdf")
