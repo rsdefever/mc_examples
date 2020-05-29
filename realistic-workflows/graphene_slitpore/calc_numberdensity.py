79d0ec7f61d59a71064b0c0efb61e608b36b150a
Ray Matsumoto <raymat@master.cl.vanderbilt.edu>
2020-05-29 13:00:01 -0400

    Add other nvt script and number density calculatino

---
+++ realistic-workflows/graphene_slitpore/calc_numberdensity.py
@@ -0,0 +1,107 @@
+import numpy as np
+import mdtraj as md
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import os
+import sys
+from mtools.gromacs.gromacs import make_comtrj
+
+
+def calc_number_density(gro_file, trj_file, area,
+        dim, box_range, n_bins, frame_range=None,maxs=None, mins=None):
+    """
+    Calculate a 1-dimensional number density profile for each residue
+
+    Parameters
+    ----------
+    gro_file: str
+        GROMACS '.gro' file to load 
+    trj_file: str
+        Trajectory to load
+    bin_width: int
+        Width (nm) of numpy histogram bins
+    dim: int
+        Dimension to calculate number density profile (0,1 or 2)
+    box_range: array
+        Range of coordinates in 'dim' to evaluate
+    frame_range: Python range() (optional)
+        Range of frames to calculate number density function over
+    maxs: array (optional)
+        Maximum coordinate to evaluate 
+    mins: array (optional)
+        Minimum coordinate to evalute
+    
+    Attributes
+    ----------
+    """
+    trj = md.load(trj_file, top=gro_file)
+    com_trj = make_comtrj(trj)
+    resnames = np.unique([x.name for x in
+               com_trj.topology.residues])
+    rho_list = list()
+    res_list = list()
+    
+    for resname in resnames:
+        sliced = com_trj.topology.select('resname {}'.format(resname))
+        trj_slice = com_trj.atom_slice(sliced)
+        if frame_range:
+            trj_slice = trj_slice[frame_range]
+        for i,frame in enumerate(trj_slice):
+            if maxs is None:
+                indices = [[atom.index for atom in compound.atoms]
+                          for compound in
+                          list(frame.topology.residues)]
+            else:
+                indices = np.intersect1d(
+                          np.intersect1d(np.where(frame.xyz[-1, :, 0]
+                              > mins[0]),
+                          np.where(frame.xyz[-1, :, 0] < maxs[0])),
+                          np.intersect1d(np.where(frame.xyz[-1, :, 1] 
+                              > box_range[0]),
+                          np.where(frame.xyz[-1, :, 1] < box_range[1])))
+
+            if frame_range:
+                if i == 0:
+                    x = np.histogram(frame.xyz[0,indices,dim].flatten(), 
+                        bins=n_bins, range=(box_range[0], box_range[1]))
+                    rho = x[0]
+                    bins = x[1]
+                else:
+                    rho += np.histogram(frame.xyz[0, indices, dim].
+                            flatten(),bins=n_bins, range=(box_range[0],
+                                box_range[1]))[0]
+            else:
+                if i == 0:
+                    x = np.histogram(frame.xyz[0,indices,dim].flatten(), 
+                        bins=n_bins, range=(box_range[0], box_range[1]))
+                    rho = x[0]
+                    bins = x[1]
+                else:
+                    rho += np.histogram(frame.xyz[0, indices, dim].
+                            flatten(),bins=n_bins, range=(box_range[0],
+                                box_range[1]))[0]
+        #rho = np.divide(rho, trj_slice.n_frames * area *
+        #        2 / n_bins)
+        rho = np.divide(rho, trj_slice.n_frames*area*(bins[1]-bins[0]))
+        rho_list.append(rho)
+        res_list.append(resname)
+
+    bin_list = bins[:-1]
+    
+    return (rho_list, bin_list, res_list)
+
+
+area = 2.95 * 2.98
+dim = 2
+box_range = [0.67, 2.17]
+n_bins = 100
+trj = md.load('equil.out.xyz', top='coords.gro')
+trj.save_trr('equil.trr')
+rho, bins, residues = calc_number_density('coords.gro', 'equil.trr', area, dim, box_range, n_bins)
+
+for res in range(1):
+    plt.plot(bins, rho[res], label='{}'.format(residues[res]))
+plt.legend()
+plt.ylabel(r'Number density ($nm^{-3}$)')
+plt.xlabel('Position in slit pore (nm)')
+plt.savefig('number-density.pdf')
---
+++ realistic-workflows/graphene_slitpore/start_nvt.py
@@ -0,0 +1,100 @@
+import mbuild as mb
+import foyer
+import mosdef_cassandra as mc
+import numpy as np
+import os
+import unyt as u
+
+from mbuild import recipes
+from foyer import Forcefield
+from mosdef_cassandra.utils.tempdir import temporary_cd
+from cassandra_slitpore.utils.utils import translate_compound, wrap_coords
+from utils import get_last_frame
+
+# Create graphene system and atom type
+graphene = recipes.GraphenePore(pore_width=1.5, pore_length=2.95, pore_depth=2.98, slit_pore_dim=2)
+ff = Forcefield('ffxml/C-spce.xml')
+
+typed_graphene = ff.apply(graphene)
+
+# Create solvent
+water = mb.load('structures/spce.mol2')
+typed_water = ff.apply(water)
+
+# Create box and species list
+box_list = [graphene]
+species_list = [typed_graphene, typed_water]
+
+# Specify mol to add in box
+mols_to_add = [[0, 296]]
+# Specify mols at start of the simulation
+mols_in_boxes = [[1, 0]]
+
+# Create MC system
+system = mc.System(box_list, species_list, mols_in_boxes=mols_in_boxes, mols_to_add=mols_to_add)
+moves = mc.MoveSet("nvt", species_list)
+
+# Set thermodynamic properties
+thermo_props = [
+    "energy_total",
+    "pressure",
+    "volume",
+    "nmols",
+    "mass_density",
+]
+
+mu = -40.0 * (u.kJ/u.mol)
+
+custom_args = {
+    "run_name": "equil",
+    "charge_style": "ewald",
+    "rcut_min": 0.2 * u.angstrom,
+    "vdw_cutoff": 10.0 * u.angstrom,
+    "units": "sweeps",
+    "properties": thermo_props,
+}
+
+
+# Cassandra scales OK up to 4-8 cores
+mc.run(
+system=system,
+moveset=moves,
+run_type="equilibration",
+run_length=100000,
+temperature=298.0 * u.K,
+**custom_args,
+)
+
+wrap_coords('equil.out.xyz', graphene.periodicity*10)
+get_last_frame('equil.out.xyz', 100000)
+
+# Load in last frame of the GCMC simulation
+gcmc_system = mb.load('last_frame.xyz')
+
+# Create separate mBuild molecules for water and graphene
+graphene = mb.Compound()
+water = mb.Compound()
+single_mol = mb.Compound()
+
+water_count = 0
+for child in gcmc_system.children:
+    if child.name == 'C':
+        graphene.add(mb.clone(child))
+    else:
+        water_count += 1
+        single_mol.add(mb.clone(child))
+        if water_count % 3 == 0:
+            single_mol.name = 'SOL'
+            single_mol.generate_bonds('O', 'H', 0, 0.3)
+            water.add(mb.clone(single_mol))
+            single_mol = mb.Compound()
+
+water.add(mb.clone(graphene))
+water.periodicity = np.array([2.9472, 2.97774175, 3.175])
+
+# Create FF object and apply to graphene and water
+ff = Forcefield('ffxml/C-spce.xml')
+
+#typed_water = ff.apply(water)
+
+water.save('coords.gro', combine='all', overwrite=True, residues=['C', 'SOL'])
