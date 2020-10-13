119b549da31062c1b972a2174b3169f6f8dc84bb
Ray Matsumoto <raymat@master.cl.vanderbilt.edu>
2020-10-13 13:18:47 -0400

    Edit mdp files and add rest of run files

---
+++ mc_examples/realistic_workflows/graphene_slitpore/md_pore/1.5nm_0pairs/run.py
@@ -0,0 +1,22 @@
+import unyt as u
+
+from mc_examples.realistic_workflows.graphene_slitpore.utils import create_system
+from mc_examples.realistic_workflows.graphene_slitpore.md_pore.runners import run_gromacs
+
+
+def main():
+
+    pore_width = 1.5 * u.nm
+    n_ion_pairs = 0
+    n_water = 75
+
+    filled_pore = create_system(pore_width,
+                             n_ion_pairs,
+                             n_water,
+    )
+
+    # Run the MD Simulations
+    run_gromacs(filled_pore) 
+
+if __name__ == "__main__":
+    main()
