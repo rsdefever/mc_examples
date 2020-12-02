5ecab0c35ebad70ea5b369ed27eab1e4e091f583
Ryan S. DeFever <r.s.defever@gmail.com>
2020-12-02 16:14:27 -0500

    Update fluid phase simulations

--- mc_examples/realistic_workflows/zeolite_adsorption/analyze_pure.py
+++ mc_examples/realistic_workflows/zeolite_adsorption/analyze_pure.py
@@ -2,160 +2,131 @@ import os
 import unyt as u
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn
 
 from mosdef_cassandra.analysis import ThermoProps
-from mpl_toolkits.axes_grid1.inset_locator import InsetPosition, inset_axes
 from scipy.stats import linregress
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+from matplotlib import rcParams
 
-plt.rc('font', family='serif')
+rcParams['font.sans-serif'] = "Arial"
+rcParams['font.family'] = "sans-serif"
 
-# Make sure the range of temperatures and chemical potentials
-# matches those for which the simulations were run
-temperatures = [298.0 * u.K, 309.0 * u.K, 350 * u.K]
-mus_adsorbate = np.arange(-49, -30, 3) * u.Unit('kJ/mol')
 
-# Create a location to output our results
-outdir="plots/"
-if not os.path.isdir(outdir):
-    os.mkdir(outdir)
-else:
-    pass
+def main():
 
-# First let's check the equilibration length. We'll plot
-# the pressure and total energy for each temperature
-property_ = "Energy_Total"
-for temperature in temperatures:
-    for mu_adsorbate in np.flip(mus_adsorbate):
-        dirname = f'pure_T_{temperature:0.1f}_mu_{mu_adsorbate:.1f}'.replace(" ", "_").replace("/", "-")
-        thermo_equil = ThermoProps(dirname + "/equil.out.prp")
-        thermo_prod = ThermoProps(dirname + "/prod.out.prp")
-        prev_plt = plt.plot(
-            thermo_equil.prop("MC_STEP"),
-            thermo_equil.prop(property_),
-            label=mu_adsorbate
+    # Make sure the range of temperatures and chemical potentials
+    # matches those for which the simulations were run
+    temperatures = [298 * u.K, 309 * u.K, 350 * u.K]
+    mus = np.arange(-49, -30, 3) * u.Unit('kJ/mol')
+    
+    # Create a location to output our results
+    outdir="plots/"
+    if not os.path.isdir(outdir):
+        os.mkdir(outdir)
+    else:
+        pass
+    
+    # First let's check the equilibration length. We'll plot
+    # the pressure and total energy for each temperature
+    for property_ in ["Energy_Total", "Pressure"]:
+        for temperature in temperatures:
+            fig, ax = plt.subplots()
+            for mu in np.flip(mus):
+                dirname = f'pure_T_{temperature:0.1f}_mu_{mu:.1f}'.replace(" ", "_").replace("/", "-")
+                thermo_equil = ThermoProps(dirname + "/equil.out.prp")
+                thermo_prod = ThermoProps(dirname + "/prod.out.prp")
+                dirname = f'pure_T_{temperature:0.1f}_mu_{mu:.1f}'.replace(" ", "_").replace("/", "-")
+                thermo_equil = ThermoProps(dirname + "/equil.out.prp")
+                thermo_prod = ThermoProps(dirname + "/prod.out.prp")
+                prev_plt = ax.plot(
+                    thermo_equil.prop("MC_STEP"),
+                    thermo_equil.prop(property_),
+                    label=mu,
+                )
+                ax.plot(
+                    thermo_prod.prop("MC_STEP"),
+                    thermo_prod.prop(property_),
+                    color=prev_plt[0]._color
+                )
+            ax.set_title(f"T = {temperature}", fontsize=16)
+            ax.set_xlabel("MC Step", fontsize=16, labelpad=15)
+            ax.set_ylabel(f"{property_}, {thermo_equil.prop(property_).units}".replace("_"," "), fontsize=16, labelpad=10)
+            ax.tick_params(which="both", direction="in", labelsize=14, top=True, right=True)
+            fig.legend()
+            fig.tight_layout(pad=2)
+            fig.savefig(outdir + f"/{property_}_T{temperature.value}K.pdf")
+    
+    # Next let's look at the number of molecules in the
+    # simulation boxes to make sure our boxes were large enough.
+    # We will only look at the production simulations
+    for temperature in temperatures:
+        fig, ax = plt.subplots()
+        for mu in np.flip(mus):
+            dirname = f'pure_T_{temperature:0.1f}_mu_{mu:.1f}'.replace(" ", "_").replace("/", "-")
+            thermo = ThermoProps(dirname + "/prod.out.prp")
+            width=5
+            ax.hist(
+                thermo.prop("Nmols"),
+                alpha=0.4,
+                align="left",
+                bins=range(15, 70, 3),
+                label=str(mu),
+                density=True
+            )
+       
+        ax.set_xlabel("Number of molecules", fontsize=16, labelpad=10)
+        ax.set_ylabel("Probability", fontsize=16, labelpad=10)
+        ax.tick_params(which="both", direction="in", labelsize=14, top=True, right=True)
+        ax.xaxis.set_minor_locator(AutoMinorLocator(n=2))
+        ax.yaxis.set_minor_locator(AutoMinorLocator(n=2))
+        fig.tight_layout(pad=2)
+        fig.legend(fontsize=16, loc="upper right", bbox_to_anchor=(0.95, 0.95))
+        fig.savefig(outdir + f"/nmols-hist_T{temperature.value}K.pdf")
+
+    # Finally let's plot the relationship between mu
+    # and pressure at all temperatures
+    fig, ax = plt.subplots()
+    for temperature in temperatures:
+        pressures = []
+        for mu in mus:
+            dirname = f'pure_T_{temperature:0.1f}_mu_{mu:.1f}'.replace(" ", "_").replace("/", "-")
+            thermo = ThermoProps(dirname + "/prod.out.prp")
+            pressures.append(np.mean(thermo.prop("Pressure", start=100000)))
+        pressures = u.unyt_array(pressures)
+    
+        # Fit a line to mu vs. P
+        slope, intercept, r_value, p_value, stderr = linregress(
+            mus.to_value('kJ/mol').flatten(),
+            y=np.log(pressures.to_value(u.bar)).flatten()
         )
-        plt.plot(
-            thermo_prod.prop("MC_STEP"),
-            thermo_prod.prop(property_),
-            color=prev_plt[0]._color
+        # Determine chemical potentials
+        fit_press = np.exp(slope * np.arange(-50,-29,1) + intercept) * u.bar
+        
+        # Plot
+        prev_plt = ax.plot(
+            mus.to_value('kJ/mol'),
+            pressures.to_value('bar'), 'o',
+            markersize=10,
         )
-    plt.title(f"T = {temperature}", fontsize=16)
-    plt.xlabel("MC Step", fontsize=16, labelpad=15)
-    plt.ylabel(f"{property_}, {thermo_equil.prop(property_).units}".replace("_"," "), fontsize=16, labelpad=10)
-    plt.legend()
-    plt.tight_layout(pad=2)
-
-    plt.savefig(outdir + f"/{property_}_T{temperature.value}K.pdf")
-    plt.close()
-
-property_ = "Pressure"
-for temperature in temperatures:
-    for mu_adsorbate in np.flip(mus_adsorbate):
-        dirname = f'pure_T_{temperature:0.1f}_mu_{mu_adsorbate:.1f}'.replace(" ", "_").replace("/", "-")
-        thermo_equil = ThermoProps(dirname + "/equil.out.prp")
-        thermo_prod = ThermoProps(dirname + "/prod.out.prp")
-        prev_plt = plt.plot(
-            thermo_equil.prop("MC_STEP"),
-            thermo_equil.prop(property_),
-            label=mu_adsorbate
+        ax.plot(
+            np.arange(-50,-29,1),
+            fit_press.to_value('bar'),
+            '-',
+            color=prev_plt[0]._color,
+            label=f"{temperature:.0f}"
         )
-        plt.plot(
-            thermo_prod.prop("MC_STEP"),
-            thermo_prod.prop(property_),
-            color=prev_plt[0]._color
-        )
-    plt.title(f"{property_}, T = {temperature}")
-    plt.xlabel("MC Step")
-    plt.ylabel(f"{property_}, {thermo_equil.prop(property_).units}")
-    plt.legend()
+    ax.set_xlabel("Chemical potential (kJ/mol)", fontsize=16, labelpad=15)
+    ax.xaxis.set_minor_locator(AutoMinorLocator(3))
+    ax.xaxis.set_major_locator(MultipleLocator(3))
+    ax.set_ylabel("Pressure (bar)", fontsize=16, labelpad=10)
+    ax.tick_params(which="both", direction="in", labelsize=14, top=True, right=True)
+    ax.set_yscale("log")
+    ax.set_xlim(-50,-30)
+    fig.legend(fontsize=16, loc="upper left", bbox_to_anchor=(0.15, 0.95))
+    fig.tight_layout(pad=2.0)
+    fig.savefig(outdir + f"/chempot_vs_pressure.pdf")
 
-    plt.savefig(outdir + f"/{property_}_T{temperature.value}K.pdf")
-    plt.close()
 
-# Next let's look at the number of molecules in the
-# simulation boxes to make sure our boxes were large enough.
-# We will only look at the production simulations
-for temperature in temperatures:
-    fig, parent_ax = plt.subplots()
-    inset_ax = inset_axes(
-        parent_ax, width="45%", height=1.0, loc="upper right",
-        bbox_to_anchor=(-0.02,-0.02,1,1), bbox_transform=parent_ax.transAxes
-    )
-    for mu_adsorbate in np.flip(mus_adsorbate):
-        dirname = f'pure_T_{temperature:0.1f}_mu_{mu_adsorbate:.1f}'.replace(" ", "_").replace("/", "-")
-        thermo = ThermoProps(dirname + "/prod.out.prp")
-        width=5
-        n = np.ceil((thermo.prop("Nmols").value.max() - thermo.prop("Nmols").value.min())/width)
-        parent_ax.hist(
-            thermo.prop("Nmols"),
-            alpha=0.6,
-            align="left",
-            bins=int(n),
-            label=mu_adsorbate,
-            density=True
-        )
-        width=1
-        n = np.ceil((thermo.prop("Nmols").value.max() - thermo.prop("Nmols").value.min())/width)
-        inset_ax.hist(
-            thermo.prop("Nmols"),
-            alpha=0.6,
-            align="left",
-            bins=int(n),
-            label=mu_adsorbate,
-            density=True
-        )
-   
-    #parent_ax.set_title(f"T = {temperature:.0f}", fontsize=16)
-    parent_ax.set_xlabel("Number of molecules", fontsize=16, labelpad=10)
-    parent_ax.set_ylabel("Probability", fontsize=16, labelpad=10)
-    parent_ax.tick_params(axis='both', labelsize=12)
-
-    inset_ax.set_xlim(-2,12)
-    inset_ax.set_xlabel("Number of molecules")
-    inset_ax.set_ylabel("Probability")
-    parent_ax.legend(loc="upper left")
-    fig.tight_layout(pad=2)
-    fig.savefig(outdir + f"/nmols-hist_T{temperature.value}K.pdf")
-    plt.close()
-
-# Finally let's plot the relationship between the chemical potential
-# and pressure at both temperatures
-for temperature in temperatures:
-    pressures = []
-    for mu_adsorbate in mus_adsorbate:
-        dirname = f'pure_T_{temperature:0.1f}_mu_{mu_adsorbate:.1f}'.replace(" ", "_").replace("/", "-")
-        thermo = ThermoProps(dirname + "/prod.out.prp")
-        pressures.append(np.mean(thermo.prop("Pressure", start=100000)))
-    pressures = u.unyt_array(pressures)
-
-    # Fit a line to mu vs. P
-    slope, intercept, r_value, p_value, stderr = linregress(
-        mus_adsorbate.to_value('kJ/mol').flatten(),
-        y=np.log(pressures.to_value(u.bar)).flatten()
-    )
-    # Determine chemical potentials
-    fit_press = np.exp(slope * np.arange(-50,-29,1) + intercept) * u.bar
-
-    prev_plt = plt.plot(
-        mus_adsorbate.to_value('kJ/mol'),
-        pressures.to_value('bar'), 'o',
-        markersize=10,
-    )
-    plt.plot(
-        np.arange(-50,-29,1),
-        fit_press.to_value('bar'),
-        '-',
-        color=prev_plt[0]._color,
-        label=f"{temperature:.0f}"
-    )
-    plt.xlabel("Chemical potential (kJ/mol)", fontsize=16, labelpad=15)
-    plt.ylabel("Pressure (bar)", fontsize=16, labelpad=10)
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-    plt.yscale('log')
-    plt.xlim(-50,-30)
-    plt.legend(fontsize=16)
-    plt.tight_layout(pad=2.0)
-
-    plt.savefig(outdir + f"/chempot_vs_pressure.pdf")
-
+if __name__ == "__main__":
+    main()
--- mc_examples/realistic_workflows/zeolite_adsorption/run_pure.py
+++ mc_examples/realistic_workflows/zeolite_adsorption/run_pure.py
@@ -1,78 +1,91 @@
+import os
 import mbuild
 import foyer
 import mosdef_cassandra as mc
 import unyt as u
-
-import os
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import linregress
 
+from scipy.stats import linregress
 from mosdef_cassandra.analysis import ThermoProps
 from mosdef_cassandra.utils.tempdir import temporary_cd
 
-# Filter some warnings -- to cleanup output for this demo
-from warnings import filterwarnings
-filterwarnings('ignore', category=UserWarning)
-from parmed.exceptions import OpenMMWarning
-filterwarnings('ignore', category=OpenMMWarning)
 
-# Create a CG methane, load and apply ff
-methane = mbuild.Compound(name='_CH4')
-ff_ads = foyer.Forcefield('resources/ffxml/adsorbates.xml')
-methane_typed = ff_ads.apply(methane)
+def main():
+    # Create a CG methane, load and apply ff
+    methane = mbuild.Compound(name="_CH4")
+    ff_ads = foyer.Forcefield("resources/ffxml/adsorbates.xml")
+    methane_ff = ff_ads.apply(methane)
 
-# Define a few custom_args that will be the same for
-# all pure phase simulations
-custom_args = {
-    "charge_style" : "none",
-    "vdw_cutoff" : 14.0 * u.angstrom,
-    "prop_freq" : 10,
-    "max_molecules" : [20000]
-}
+    # Define a few keyword args that will be the
+    # same for all pure phase simulations
+    custom_args = {
+        "charge_style": "none",
+        "vdw_cutoff": 14.0 * u.angstrom,
+        "prop_freq": 10,
+    }
 
-# Run pure phase simulations at two temperatures
-# Use the same range of chemical potentials at both temperatures
-temperatures = [298.0 * u.K, 309.0 * u.K, 350 * u.K]
-mus_adsorbate = np.arange(-49, -30, 3) * u.Unit('kJ/mol')
+    # Define temperatures and chemical potential values
+    # for the single phase GCMC simulations
+    temperatures = [298 * u.K, 309 * u.K, 350 * u.K]
+    mus = np.arange(-49, -30, 3) * u.Unit("kJ/mol")
 
-for temperature in temperatures:
-    for mu_adsorbate in mus_adsorbate:
-        print(f"\nRun simulation: T = {temperature}, mu = {mu_adsorbate}\n")
-        dirname = f'pure_T_{temperature:0.1f}_mu_{mu_adsorbate:.1f}'.replace(" ", "_").replace("/", "-")
-        if not os.path.isdir(dirname):
-            os.mkdir(dirname)
-        else:
-            pass
-        with temporary_cd(dirname):
-            species_list = [methane_typed]
-            if mu_adsorbate < -37:
-                boxl = 22. # nm
-            else:
-                boxl = 5. # nm
-            box_list = [mbuild.Box([boxl,boxl,boxl])]
+    # Loop over temperatures and mus and run simulations
+    for temperature in temperatures:
+        for mu in mus:
+
+            # For each simulation -- first estimate
+            # the box volume to target ~40 molecules
+            n_methane_target = 40
+            beta = 1.0 / (u.kb * temperature)
+            mass = 16.04 * u.amu
+            debroglie = np.sqrt(2 * np.pi * u.hbar ** 2 * beta / mass)
+            vol = n_methane_target * debroglie ** 3 * np.exp(-beta.to("mol/kJ") * mu)
+            boxl = (vol ** (1.0 / 3.0)).to_value("nm")
+            if boxl < 2.0 * custom_args["vdw_cutoff"].to_value("nm"):
+                boxl = 2.0 * custom_args["vdw_cutoff"].to_value("nm")
+
+            # Define the species list, box list, system, moveset
+            # We start with an empty box
+            species_list = [methane_ff]
+            box_list = [mbuild.Box([boxl, boxl, boxl])]
             system = mc.System(box_list, species_list)
-            moveset = mc.MoveSet('gcmc', species_list)
-    
-            mc.run(
-                system=system,
-                moveset=moveset,
-                run_type="equil",
-                run_length=50000,
-                temperature=temperature,
-                run_name='equil',
-                chemical_potentials = [mu_adsorbate],
-                **custom_args,
-            )
-    
-            mc.restart(
-                system=system,
-                moveset=moveset,
-                run_type="prod",
-                run_length=400000,
-                temperature=temperature,
-                run_name='prod',
-                restart_name='equil',
-                chemical_potentials = [mu_adsorbate],
-                **custom_args,
-            )
+            moveset = mc.MoveSet("gcmc", species_list)
+
+            # Create a new directory, temporary cd, and run
+            # the equilibration and production simulations!
+            print(f"\nRun simulation: T = {temperature}, mu = {mu}\n")
+            dirname = f"pure_T_{temperature:0.1f}_mu_{mu:.1f}".replace(
+                " ", "_"
+            ).replace("/", "-")
+            if not os.path.isdir(dirname):
+                os.mkdir(dirname)
+            else:
+                pass
+            with temporary_cd(dirname):
+                mc.run(
+                    system=system,
+                    moveset=moveset,
+                    run_type="equil",
+                    run_length=50000,
+                    temperature=temperature,
+                    run_name="equil",
+                    chemical_potentials=[mu],
+                    **custom_args,
+                )
+
+                mc.restart(
+                    system=system,
+                    moveset=moveset,
+                    run_type="prod",
+                    run_length=400000,
+                    temperature=temperature,
+                    run_name="prod",
+                    restart_name="equil",
+                    chemical_potentials=[mu],
+                    **custom_args,
+                )
+
+
+if __name__ == "__main__":
+    main()
